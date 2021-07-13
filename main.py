1047f62904fe467f3784affdb6abd773e7ccf8fa
Thomson2412 <thomasm.w.fink@gmail.com>
2021-07-13 15:47:57 +0200

    Update

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ import cv2
 import SceneDetectionAudio
 
 if __name__ == '__main__':
-    SceneDetectionAudio.update_object_scene_detection_files(
-        "soundnet/mp3/",
-        "soundnet",
-        "data/test/audio_scene/object_audio_detection.json",
-        "data/test/audio_scene/object_scene_detection.json",
-    )
-
-    audio_paths = SceneDetectionAudio.get_audio_for_scene("data/test/audio_scene/object_scene_detection.json", "/r/railroad_track")
-    print(audio_paths)
+    # SceneDetectionAudio.update_object_scene_detection_files(
+    #     "soundnet/mp3/",
+    #     "soundnet",
+    #     "data/test/audio_scene/object_audio_detection.json",
+    #     "data/test/audio_scene/object_scene_detection.json",
+    # )
+    #
+    # audio_paths = SceneDetectionAudio.get_audio_for_scene("data/test/audio_scene/object_scene_detection.json", "/r/railroad_track")
+    # print(audio_paths)
 
     # ImageConverter.convert_paintings_to_txt_bulk("data/painter_by_numbers_scene_correct/", "converted/")
 
@@ -21,20 +21,21 @@ if __name__ == '__main__':
 
     # ImageConverter.convert_painting_to_presentation_bulk("data/test/", "data/presentation/", False, True, False, True)
 
-    # ImageConverter.convert_painting_to_presentation_bulk(
-    #     "/mnt/datadrive/projects/thesis/Datasets/Paintings/painter_by_numbers_scene_correct/",
-    #     "data/presentation_saliency/",
-    #     True,
-    #     False,
-    #     True,
-    #     True,
-    #     True,
-    #     False)
+    ImageConverter.convert_painting_to_presentation_bulk(
+        "/mnt/datadrive/projects/thesis/Datasets/Paintings/painter_by_numbers_scene_correct/",
+        "data/presentation_saliency/",
+        True,
+        False,
+        True,
+        True,
+        True,
+        False)
 
     # ImageConverter.convert_painting_to_presentation_bulk(
     #     "data/test",
     #     "data/test/",
     #     True,
+    #     False,
     #     True,
     #     False,
     #     True,
@@ -45,6 +46,9 @@ if __name__ == '__main__':
     #     "data/test/allegorical_painting-Arthur_Streeton-Spirit_of_the_drought-1895-65154.jpg",
     #     "data/test/",
     #     saliency_coarse,
+    #     True,
+    #     None,
+    #     False,
     #     True,
     #     True,
     #     True,
